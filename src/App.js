9b7979ddb75a094f568b60c99ea9b85092360849
webteam-hub <demouserhello112@gmail.com>
2021-04-28 18:55:12 +0530

    Sec

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TransaprentButton from './transparentButton';
 function App() {
   return (
     <div className="App">
-        <TransaprentButton buttonTitle='Click Me' onClick= {() => (alert("Hello! I am an alert box!!!"))}
+        <TransaprentButton buttonTitle='Click Me' onClick= {() => (alert("Hello! I am an alert box!!"))}
         />
     </div>
   );
